ab7f181b333e57576281fc84c173423bf1d2e40b
Juan Mesa <linuxven@gmail.com>
2015-12-31 20:18:55 -0430

    Fix user data to perform tasks as ec2-user

--- user-data.sh
+++ user-data.sh
@@ -5,9 +5,7 @@ yum update -y
 yum install -y git
 
 # Configure and clone git repository
-ssh-keyscan -H github.com >> /root/.ssh/known_hosts
-git clone https://github.com/overdrive3000/mybb-deploy-node.git
-git clone https://github.com/overdrive3000/mybb.git
+su - ec2-user -c "git clone https://github.com/overdrive3000/mybb-deploy-node.git /home/ec2-user/stack_deploy"
 
 # Configure AWS Cli
 aws configure set region us-west-2
@@ -22,10 +20,8 @@ aws s3 cp /tmp/mybb.zip s3://jmesa-crossover-deployment/ebs-app-bundle/mybb.zip
 cd ..
 
 # Install node.js
-su - ec2-user -c "ssh-keyscan -H github.com >> /home/ec2-user/.ssh/known_hosts"
 yum -y groupinstall "Development Tools"
 su - ec2-user -c "curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash"
 su - ec2-user -c "nvm install stable"
 # Change the following line with the right repo
-su - ec2-user -c "git clone https://github.com/overdrive3000/aws-infra-deployment.git /home/ec2-user/stack_deploy"
 su - ec2-user -c "cd /home/ec2-user/stack_deploy; npm install; cd"
