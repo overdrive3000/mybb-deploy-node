1e8e0b231ff60d40182f6753b90b2e83f83c3138
Juan Mesa <linuxven@gmail.com>
2016-01-16 10:00:16 -0430

    Fixed tabs

--- config/settings.js
+++ config/settings.js
@@ -2,20 +2,19 @@ import path from 'path';
 
 const timestamp = new Date().getTime();
 
-const projectRoot						= path.dirname(__dirname);
+const projectRoot           = path.dirname(__dirname);
 
 // Basic configuration
-const keyName								= 'overdrive'; // SSH Key Pair to use
-const stackName							= 'MyBBForum'; // Stack Name
-// const templateURL						= 'https://s3.amazonaws.com/jmesa-crossover-deployment-01/cf-templates/mybb-master.json'; // Master template URL
-const assetsBucketPrefix		= 'jmesa-crossover-deployment';
+const keyName               = 'overdrive'; // SSH Key Pair to use
+const stackName             = 'MyBBForum'; // Stack Name
+const assetsBucketPrefix    = 'jmesa-crossover-deployment';
 
 // SNS Topic Configuration
-const operatorEMail					= 'linuxven@gmail.com';
+const operatorEMail         = 'linuxven@gmail.com';
 
 // VPC configuration
-const vpcAvailabilityZone1	= 'us-west-2a';
-const vpcAvailabilityZone2	= 'us-west-2c';
+const vpcAvailabilityZone1  = 'us-west-2a';
+const vpcAvailabilityZone2  = 'us-west-2c';
 
 // RDS database configuration
 const dbName              = 'mybb_db'; // Database Name
@@ -23,25 +22,25 @@ const dbPassword          = 'nonsecurepassword'; // Database Password
 const dbUser              = 'admin'; // Database User
 
 // Elastic Beanstalk configuration
-const zipKey							= 'ebs-app-bundle/mybb.zip'; // ZIP bundle to be deployed in Elastic Beanstalk
-const developerName				= `jmesa-${timestamp}`;
-const environmentName			= 'production';
-const applicationName			= 'MyBB-Forum';
+const zipKey              = 'ebs-app-bundle/mybb.zip'; // ZIP bundle to be deployed in Elastic Beanstalk
+const developerName       = `jmesa-${timestamp}`;
+const environmentName     = 'production';
+const applicationName     = 'MyBB-Forum';
 
 
 export default {
-  keyName:							keyName,
-  stackName:						stackName,
-	templateURL:					templateURL,
-	assetsBucketPrefix:		assetsBucketPrefix,
-	operatorEMail:				operatorEMail,
-	vpcAvailabilityZone1:	vpcAvailabilityZone1,
-	vpcAvailabilityZone2:	vpcAvailabilityZone2,
-  dbName:								dbName,
-  dbPassword:						dbPassword,
-  dbUser:								dbUser,
-	zipKey:								zipKey,
-	developerName:				developerName,
-	environmentName:			environmentName,
-	applicationName:			applicationName
+  keyName:              keyName,
+  stackName:            stackName,
+  templateURL:          templateURL,
+  assetsBucketPrefix:   assetsBucketPrefix,
+  operatorEMail:        operatorEMail,
+  vpcAvailabilityZone1: vpcAvailabilityZone1,
+  vpcAvailabilityZone2: vpcAvailabilityZone2,
+  dbName:               dbName,
+  dbPassword:           dbPassword,
+  dbUser:               dbUser,
+  zipKey:               zipKey,
+  developerName:        developerName,
+  environmentName:      environmentName,
+  applicationName:      applicationName
 }
